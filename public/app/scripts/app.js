a953a050ac552e09b0e6f7e539942d0be3392c8a
David Chang <dchang.me@gmail.com>
2013-05-06 00:00:28 -0700

    with localStorage, some tweaks now

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('publicApp', [])
+angular.module('publicApp', ['LocalStorageModule'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -1,23 +1,27 @@
 'use strict';
 
 angular.module('publicApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-      $scope.lists = [{
-          name: 'list1',
-          byDay: true,
-          days: []
-          //days is like new Date().valueOf() / 1000, on a scale of 0 to 100
-      }, {
-          name: 'list2',
-          byDay: false,
-          days: []
-      }];
+  .controller('MainCtrl', ['$scope', 'localStorageService', function ($scope, ls) {
+      if(ls.get('lists'))
+          $scope.lists = JSON.parse(ls.get('lists'));
+      else {
+          $scope.lists = [{
+              name: 'list1',
+              byDay: true,
+              days: []
+              //days is like new Date().valueOf() / 1000, on a scale of 0 to 100
+          }, {
+              name: 'list2',
+              byDay: false,
+              days: []
+          }];
+      }
 
       $scope.showAddNewList = false;
 
       $scope.newName = '';
       $scope.newByDay = true;
-      $scope.save = function() {
+      $scope.createNewList = function() {
           if(!$scope.newName)
               return;
 
@@ -25,6 +29,7 @@ angular.module('publicApp')
           $scope.newName = '';
           $scope.newByDay = true;
           $scope.showAddNewList = false;
+          save();
       };
 
       function truncatedDay(d) {
@@ -32,8 +37,13 @@ angular.module('publicApp')
           return new Date(d.toString().split(' ', 4).join(' ').toString()).valueOf();
       }
 
+      function save() {
+          ls.add('lists', JSON.stringify($scope.lists));
+      }
+
       $scope.statusChange = function(i, isGood) {
           $scope.lists[i].days.push({ realTime: new Date(), dayTime: truncatedDay(), good: isGood});
+          save();
       }
 
       $scope.showStatusForToday = function(i) {
@@ -41,5 +51,23 @@ angular.module('publicApp')
           return !_.find($scope.lists[i].days, function(day) {
               return day.dayTime == truncatedToday;
           });
+      };
+
+      $scope.getStatusForToday = function(i) {
+          var truncatedToday = truncatedDay();
+          var day = _.find($scope.lists[i].days, function(day) {
+              return day.dayTime == truncatedToday;
+          });
+
+          return day ? day.good : false;
+      };
+      $scope.undoToday = function(i) {
+          var truncatedToday = truncatedDay();
+          var day = _.find($scope.lists[i].days, function(day) {
+              return day.dayTime == truncatedToday;
+          });
+
+          var index = $scope.lists[i].days.indexOf(day);
+          $scope.lists[i].days.splice(index, 1);
       };
   }]);
