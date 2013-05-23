540e5bfc4109bcfd051b6d0b908233383d9b9590
David Chang <dchang.me@gmail.com>
2013-05-23 09:07:40 -0700

    added the log in view/controller

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -3,6 +3,10 @@
 angular.module('publicApp', ['LocalStorageModule'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
+      .when('/login', {
+        templateUrl: 'views/logInView.html',
+        controller: 'LogInCtrl'
+      })
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
