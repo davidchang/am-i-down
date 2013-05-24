74256a9d57ca145a1319f67a66462fc1ed255d6c
David Chang <dchang.me@gmail.com>
2013-05-24 09:09:28 -0700

    log out functionality

--- routes.js
+++ routes.js
@@ -5,16 +5,16 @@ module.exports.setRoutes = function(app, passport) {
     app.get('/auth/facebook', passport.authenticate('facebook'));
     app.get('/auth/facebook/callback', passport.authenticate('facebook', {
         successRedirect: '/#/main',
-        failureRedirect: '/' }));
+        failureRedirect: '/#/login' }));
 
     app.get('/auth/twitter', passport.authenticate('twitter'));
     app.get('/auth/twitter/callback', passport.authenticate('twitter', {
         successRedirect: '/#/main',
-        failureRedirect: '/' }));
+        failureRedirect: '/#/login' }));
 
     app.get('/logout', function(req, res){
         req.logout();
-        res.redirect('/');
+        res.redirect('/#/login');
     });
 
     /* REST */
