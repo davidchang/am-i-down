991edbc0ee031946e606af63c3b0fe8ebe43553f
David Chang <dchang.me@gmail.com>
2013-06-05 19:56:15 -0700

    things are kinda working now, which is awesome

--- public/app/scripts/directives/listObj.js
+++ public/app/scripts/directives/listObj.js
@@ -1,31 +1,81 @@
 'use strict';
 
 angular.module('publicApp')
-  .directive('metric', function () {
+  .directive('metric', ['REST', function (REST) {
     return {
       templateUrl: 'partials/listObj.html',
       restrict: 'E',
-      scope: { listObj: '=', index: '@' },
-      compile: function postCompile(scope, element, attrs) {
-        return {
-          post: function(scope) {
-            console.log(scope);
-            console.log(scope.listObj);
-            console.log(scope.index);
+      scope: { data: '=', index: '@' },
+      link: function postLink($scope, element, attrs) {
+        function truncatedDay(d) {
+          d = d || new Date();
+          return new Date(d.toString().split(' ', 4).join(' ').toString()).valueOf();
+        }
 
-            function truncatedDay(d) {
-              d = d || new Date();
-              return new Date(d.toString().split(' ', 4).join(' ').toString()).valueOf();
+        function isValidDate(date)
+        {
+            var matches = /^(\d{2})[-\/](\d{2})[-\/](\d{4})$/.exec(date);
+            if (matches == null) return false;
+            var d = matches[2];
+            var m = matches[1] - 1;
+            var y = matches[3];
+            var composedDate = new Date(y, m, d);
+            return (composedDate.getDate() == d &&
+                    composedDate.getMonth() == m &&
+                    composedDate.getFullYear() == y) ? composedDate : false;
+        }
+
+        function save() {
+          REST.saveLists($scope.lists);
+        }
+
+        $scope.showStatusForToday = function(i) {
+          var truncatedToday = truncatedDay();
+          return !_.find($scope.data.days, function(day) {
+            return day.dayTime == truncatedToday;
+          });
+        };
+
+        $scope.statusChange = function(isGood, dateToUse) {
+            var validDate = isValidDate($scope.data.backfillDate);
+            if(dateToUse && !validDate) {
+              console.log('error');
+              return;
             }
 
-            scope.showStatusForToday = function(i) {
-              console.log(i);
-              var truncatedToday = truncatedDay();
-              return !_.find(scope.listObj.days, function(day) {
+            var time = dateToUse ? validDate : new Date();
+            var truncatedBackfillDay = truncatedDay(time);
+            var day = _.find($scope.data.days, function(day) {
+                return day.dayTime == truncatedBackfillDay;
+            });
+            if(day)
+              day.good = isGood;
+            else
+              $scope.data.days.push({ realTime: time, dayTime: truncatedBackfillDay, good: isGood});
+
+            save();
+            $scope.data.backfillDate = '';
+        }
+
+        $scope.getStatusForToday = function(i) {
+            var truncatedToday = truncatedDay();
+            var day = _.find($scope.data.days, function(day) {
                 return day.dayTime == truncatedToday;
-              });
-            };
-          }}
+            });
+
+            return day ? day.good : false;
+        };
+        $scope.undoToday = function(i) {
+            var truncatedToday = truncatedDay();
+            var day = _.find($scope.data.days, function(day) {
+                return day.dayTime == truncatedToday;
+            });
+
+            var index = $scope.data.days.indexOf(day);
+            $scope.data.days.splice(index, 1);
+
+            save();
+        };
       }
     };
-  });
+  }]);
