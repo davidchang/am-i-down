66b340374855fddb65994de86d6b27c47364e7c3
David Chang <dchang.me@gmail.com>
2013-06-14 08:44:47 -0700

    Suggestions now exist on the new metric form

--- public/app/scripts/directives/newMetricForm.js
+++ public/app/scripts/directives/newMetricForm.js
@@ -9,6 +9,19 @@ angular.module('publicApp')
       link: function postLink($scope, element, attrs) {
 
         $scope.newList = {};
+        $scope.suggestions = [
+          { name: 'Porn', desiredOutcome: 'Didn\'t look at porn', oppositeOutcome: 'Looked at porn' },
+          { name: 'Fights', desiredOutcome: 'Didn\'t fight with my wife', oppositeOutcome: 'Fought with my wife' },
+          { name: 'Reddit', desiredOutcome: 'Didn\'t go on Reddit', oppositeOutcome: 'Went on Reddit' },
+          { name: 'Digg', desiredOutcome: 'Didn\'t go on Digg', oppositeOutcome: 'Went on Digg' },
+          { name: 'Programming', desiredOutcome: 'Programmed recreationally', oppositeOutcome: 'Didn\'t program recreationally' },
+          { name: 'Sex', desiredOutcome: 'Had sex!', oppositeOutcome: 'Didn\'t have sex :/' },
+          { name: 'Running', desiredOutcome: 'Went on a run', oppositeOutcome: 'Didn\'t go on a run' }
+        ];
+
+        $scope.changeToNewList = function(suggestion) {
+          $scope.newList = suggestion;
+        }
 
         function save() {
           $rootScope.$broadcast('saveListData');
