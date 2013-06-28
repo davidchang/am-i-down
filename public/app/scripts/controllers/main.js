ef8eca204c053ea1c3401404c86db93f8c208c64
David Chang <dchang.me@gmail.com>
2013-06-28 00:20:01 -0700

    Everything hooked up properly

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -31,13 +31,35 @@ angular.module('publicApp')
       
       $scope.lastWeek = [];
       var d = new Date();
-      d.setDate(d.getDate() - 7);
+      d = new Date(d.getFullYear(), d.getMonth(), d.getDate() - 7);
       for(var i = 0; i < 7; ++i && d.setDate(d.getDate() + 1)) {
-        $scope.lastWeek.push( d.getDate() );
+        $scope.lastWeek.push( { date: d.getDate(), timestamp : d.valueOf() } );
       }
 
-      $scope.changeStatus = function(listObj, index) {
-        console.log(listObj);
-        console.log(index);
+      $scope.changeStatus = function(listObj, date) {
+        var metric = _.findWhere($scope.lists, {name: listObj.name});
+        var day = _.findWhere(metric.days, {dayTime: date.timestamp});
+        var state = getClassColor(listObj, date);
+
+        if(!state)
+          metric.days.push({ dayTime: date.timestamp, good: true });
+        else if(state == 'green')
+          day.good = false;
+        else {
+          metric.days = _.reject(metric.days, function(day) {
+            return day.dayTime == date.timestamp;
+          });
+        }
+
+        save();
+      }
+
+      function getClassColor(listObj, date) {
+        var day = _.findWhere(listObj.days, {dayTime: date.timestamp});
+        return day ? day.good ? 'green' : 'red' : '';
+      }
+
+      $scope.getClass = function(listObj, date) {
+        return 'box ' + getClassColor(listObj, date);
       }
   }]);
